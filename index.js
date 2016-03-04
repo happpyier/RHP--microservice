8c81e98d84d3dcb1acd39d2a7e32f686ed0ea7da
Thomas Bernfeld <happpyier@gmail.com>
2016-03-04 16:12:48 -0500

    Added files via upload

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ var express = require('express');
 var app = express(); 
 var path = require("path");
 var os = require('os');
-app.set('port', (process.env.PORT || 5000));
-//format is {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}
-app.get('/', function(request, response) {
-	var requestObjects = Object.keys(request.client.server);
 	var prerequestedItem = os.networkInterfaces()
 	var requestedItem = JSON.stringify(prerequestedItem);
 	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
-	response.send('Welcome to my rhb--microservice page<br/>The request objects are<br/>'+requestObjects+'<br/>The netAddress is<br/>'+netAddress); 
+	var netLanguage = JSON.stringify(os.type());
+	var netOS = JSON.stringify(os.type());
+app.set('port', (process.env.PORT));
+//format is {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}
+app.get('/', function(request, response) {
+	var requestObjects = Object.keys(request.client.server);
+	/*
+	var prerequestedItem = os.networkInterfaces()
+	var requestedItem = JSON.stringify(prerequestedItem);
+	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
+	var netLanguage = JSON.stringify(os.type());
+	var netOS = JSON.stringify(os.type());
+	*/
+	response.send(requestedItem+'<br/><br/>'+'{\"ipaddrers\":'+netAddress+',\"language\":'+netLanguage+',\"software\":'+netOS+''+'<br/> {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}');
+	
 });
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
