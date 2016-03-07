0a96160b3751c88e34bf6814c1265e0cc8c18d20
Thomas Bernfeld <happpyier@gmail.com>
2016-03-07 10:41:00 -0500

    Final Version

--- index.js
+++ index.js
@@ -2,23 +2,16 @@ var express = require('express');
 var app = express(); 
 var path = require("path");
 var os = require('os');
-	var prerequestedItem = os.networkInterfaces()
-	var requestedItem = JSON.stringify(prerequestedItem);
-	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
-	var netLanguage = JSON.stringify(os.type());
-	var netOS = JSON.stringify(os.type());
 app.set('port', (process.env.PORT));
-//format is {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}
 app.get('/', function(request, response) {
-	var requestObjects = Object.keys(request.client.server);
-	/*
+	var ip = request.headers['x-forwarded-for'];
+	var requestObjectsParsed = JSON.stringify(ip);
 	var prerequestedItem = os.networkInterfaces()
 	var requestedItem = JSON.stringify(prerequestedItem);
-	var netAddress = JSON.stringify(prerequestedItem.eth0[0].address);
-	var netLanguage = JSON.stringify(os.type());
-	var netOS = JSON.stringify(os.type());
-	*/
-	response.send(requestedItem+'<br/><br/>'+'{\"ipaddrers\":'+netAddress+',\"language\":'+netLanguage+',\"software\":'+netOS+''+'<br/> {"ipaddress":"107.144.151.227","language":"en-US","software":"Windows NT 6.1; WOW64"}');
+	var netAddress = JSON.stringify(ip);
+	var netLanguage = JSON.stringify((request.headers['accept-language']).substr(0, 5));
+	var netOS = JSON.stringify((request.headers['user-agent']).substr(13, 21));
+	response.send('{\"ipaddress\":'+netAddress+',\"language\":'+netLanguage+',\"software\":'+netOS+'}');
 	
 });
 app.listen(app.get('port'), function() {
